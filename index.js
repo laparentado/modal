51fa39fbc86cb04180e280e637e12ef80678acf8
Lori Aparentado <loriaparentado@lori-aparentados-macbook.local>
2018-04-25 16:27:26 -0400

    modal made

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-setTimeout(function(){ alert("MY MODAL"); }, 3000);
+var modal = document.getElementsByClassName("modal")[0];
+var modalContent = document.getElementsByClassName("modalContent")[0];
+var close = document.getElementsByClassName("close")[0];
+
+setTimeout (function(){
+  modal.style.display = "block";
+}, 3000);
+
+close.onclick = function(){
+  modal.style.display = "none";
+}
